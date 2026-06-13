a8b56c8753d977b03e8953aedb26a4526695bc2a
Sahil Sinha <sahilsinha.dar@gmail.com>
2026-06-13 21:31:49 +0530

    fix: ci triggers and example weaver url
    - fixed release and deploy triggers
    - using remove weaver url for examples

--- examples/vite.config.js
+++ examples/vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite';
-import path from 'path';
 
 export default defineConfig({
 	root: '.',
 	server: {
 		port: 3000,
 		cors: true
-	},
-	resolve: {
-		alias: {
-			'@neomorph/weaver': path.resolve(__dirname, '../build/weaver/1.0.0/index.js')
-		}
 	}
 });
